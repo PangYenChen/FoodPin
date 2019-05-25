7c2ef5b354f8d3dac68c3ffae7c822317f88942b
PangYen Chen <cpy4869@gmail.com>
2019-05-25 11:30:23 +0800

    作業十修正

--- FoodPin/RestaurantTableViewController.swift
+++ FoodPin/RestaurantTableViewController.swift
@@ -22,6 +22,7 @@ class RestaurantTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.cellLayoutMarginsFollowReadableWidth = true
     }
 
     // MARK: - Table view data source
